febee02a930bd7a7cda75ea5d986a8edd78898a3
Daniel B. <124447854+SootDan@users.noreply.github.com>
2024-11-20 12:23:36 +0100

    web version

--- Player.gd
+++ Player.gd
@@ -1,33 +1,33 @@
-extends Node3D
+extends CharacterBody3D
 
-var velocity := 2.0
-var is_on_floor := true
-@onready var start_y = position.y
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var target_velocity := Vector3.ZERO
+@export var speed := 1.0
+@export var fall_acceleration := 75.0
+@onready var start_y := position.y
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_pressed("Forward"):
-		position.z += velocity * delta
-	if Input.is_action_pressed("Left"):
-		position.x += velocity * delta
-	if Input.is_action_pressed("Right"):
-		position.x -= velocity * delta
-	if Input.is_action_pressed("Backward"):
-		position.z -= velocity * delta
-	if Input.is_action_pressed("Jump") and is_on_floor:
-		position.y += velocity
-
-	if not is_on_floor:
-		position.y -= velocity * 0.5 * delta
-	is_on_floor = position.y <= start_y
-
-func _input(event):
-	if event.is_action("Backward"):
-		pass
-	pass
+func _physics_process(delta):
+	var direction := Vector3.ZERO
 	
+	if Input.is_action_pressed("Right"):
+		direction.x += speed
+	if Input.is_action_pressed("Left"):
+		direction.x -= speed
+	if Input.is_action_pressed("Forward"):
+		direction.z -= speed
+	if Input.is_action_pressed("Backward"):
+		direction.z += speed
+
+	if Input.is_action_pressed("Jump") and is_on_floor():
+		direction.y += speed * 5
+	
+	if direction != Vector3.ZERO:
+		$Patrick.basis = Basis.looking_at(direction)
+	
+	target_velocity = direction * speed
+	
+	if not is_on_floor():
+		target_velocity.y -= fall_acceleration * delta
+
+	velocity = target_velocity
+	move_and_slide()
